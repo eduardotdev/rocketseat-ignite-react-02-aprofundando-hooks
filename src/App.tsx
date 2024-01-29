ad0629560dbf94fa19cd6c64228c9196f71235a9
Eduardo Tavares <eduardotdev.21@gmail.com>
2024-01-29 00:44:14 -0300

    react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
@@ -7,6 +9,9 @@ export function App() {
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
         <GlobalStyle />
       </ThemeProvider>
     </>
