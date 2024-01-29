f605ab871bc07edb8231f27e71653d5101fd6145
Eduardo Tavares <eduardotdev.21@gmail.com>
2024-01-29 00:39:03 -0300

    configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <>
-      <GlobalStyle />
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+      </ThemeProvider>
     </>
   )
 }
